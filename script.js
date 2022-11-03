f8d167fe375bd41e1368efffe6fd07c8645fd5a7
Beatriz Ortega <114701184+Beatriz-Ortega@users.noreply.github.com>
2022-11-02 21:26:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -48,66 +48,10 @@ function mark(n){
     document.getElementById("op"+n).classList.toggle("selected");
 }
 
+//Marcador de botones//
 
-
-//funcion check on hoover para proyectos//
-
-document.querySelector(".box1").addEventListener("click", box_selected1);
-document.querySelector(".box2").addEventListener("click", box_selected2);
-document.querySelector(".box3").addEventListener("click", box_selected3);
-document.querySelector(".box4").addEventListener("click", box_selected4);
-
-4
-//declarando variables
-
-//box1
-
-box1 = document.querySelector(".box1");
-check_bg1 = document.querySelector(".box1 .check");
-icon_check1 = document.querySelector(".box1 .fa-check");
-
-//box2
-
-box2 = document.querySelector(".box2");
-check_bg2 = document.querySelector(".box2 .check");
-icon_check2 = document.querySelector(".box2 .fa-check");
-
-//box3
-
-box3 = document.querySelector(".box3");
-check_bg3 = document.querySelector(".box3 .check");
-icon_check3 = document.querySelector(".box3 .fa-check");
-
-//box4
-
-box4 = document.querySelector(".box4");
-check_bg4 = document.querySelector(".box4 .check");
-icon_check4 = document.querySelector(".box4 .fa-check");
-
-function box_selected1 () {  
-    window.open('https://github.com/Beatriz-Ortega/Calculadora', '_blank');
-    box1.classList.toggle('box-selected');
-    check_bg1.classList.toggle('check-selected');
-    icon_check1.classList.toggle('icon-check');
+function selec(n){
+    document.getElementById("box"+n).classList.add("box-selected");
 }
 
-function box_selected2 () {
-    window.open('https://github.com/Beatriz-Ortega/Api-Fetch', '_blank');
-    box2.classList.toggle('box-selected');
-    check_bg2.classList.toggle('check-selected');
-    icon_check2.classList.toggle('icon-check');
-}
 
-function box_selected3 () {
-    window.open('https://github.com/Beatriz-Ortega/carrito-de-compra', '_blank');
-    box3.classList.toggle('box-selected');
-    check_bg3.classList.toggle('check-selected');
-    icon_check3.classList.toggle('icon-check');
-}
-
-function box_selected4 () {
-    window.open('https://github.com/Beatriz-Ortega/Beatriz-Ortega.github.io', '_blank');
-    box4.classList.toggle('box-selected');
-    check_bg4.classList.toggle('check-selected');
-    icon_check4.classList.toggle('icon-check');
-}
